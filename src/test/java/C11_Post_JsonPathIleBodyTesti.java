9f942ba7dd00ba5241b0ecb17b8e5367cefa3743
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-12-24 12:37:26 +0100

    14.ders  sonu

---
+++ src/test/java/C11_Post_JsonPathIleBodyTesti.java
@@ -0,0 +1,83 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C11_Post_JsonPathIleBodyTesti {
+
+    @Test
+    public void test01(){
+        /*
+        https://restful-booker.herokuapp.com/booking url’ine
+        asagidaki body'ye sahip bir POST request gonderdigimizde
+            {
+            "firstname" : "Ahmet",
+            "lastname" : “Bulut",
+            "totalprice" : 500,
+            "depositpaid" : false,
+            "bookingdates" : {
+                "checkin" : "2023-01-10",
+                "checkout" : "2023-01-20"},
+            "additionalneeds" : "wi-fi"
+            }
+
+         donen Response’un,
+            status code’unun 200,
+            ve content type’inin application-json,
+            ve response body’sindeki
+                "firstname“in,"Ahmet",
+                ve "lastname“in, "Bulut",
+                ve "totalprice“in,500,
+                ve "depositpaid“in,false,
+                ve "checkin" tarihinin 2023-01-10
+                ve "checkout" tarihinin 2023-01-20
+                ve "additionalneeds“in,"wi-fi"
+             oldugunu test edin
+
+         */
+
+        // 1- endpoint ve request body olustur
+
+        String url= "https://restful-booker.herokuapp.com/booking";
+
+        JSONObject requestBody= new JSONObject();
+        JSONObject rezervasyonTarihleriJson= new JSONObject();
+
+        rezervasyonTarihleriJson.put("checkin","2023-01-10");
+        rezervasyonTarihleriJson.put("checkout","2023-01-20");
+
+        requestBody.put("firstname","Ahmet");
+        requestBody.put("lastname","Bulut");
+        requestBody.put("totalprice",500);
+        requestBody.put("depositpaid",false);
+        requestBody.put("bookingdates",rezervasyonTarihleriJson);
+        requestBody.put("additionalneeds","wi-fi");
+
+        // 2- expected data olustur
+        // 3- request gonder, donen response'i kaydet
+
+        Response response = given().contentType(ContentType.JSON)
+                            .when().body(requestBody.toString())
+                            .post(url);
+
+        // 4- Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("booking.firstname", equalTo("Ahmet"),
+                        "booking.lastname",equalTo("Bulut"),
+                        "booking.totalprice",equalTo(500),
+                        "booking.depositpaid",equalTo(false),
+                        "booking.bookingdates.checkin",equalTo("2023-01-10"),
+                        "booking.bookingdates.checkout",equalTo("2023-01-20"));
+
+
+    }
+}
---
+++ src/test/java/C12_Get_ResponseBodyTestiListKullanimi.java
@@ -0,0 +1,45 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class C12_Get_ResponseBodyTestiListKullanimi {
+
+    @Test
+    public void test01(){
+        /*
+            http://dummy.restapiexample.com/api/v1/employees url'ine
+            bir GET request yolladigimizda
+                donen Response'in
+                    status code'unun 200,
+                    ve content type'inin Aplication.JSON,
+                    ve response body'sindeki
+                    employees sayisinin 24
+                    ve employee'lerden birinin "Ashton Cox"
+                    ve girilen yaslar icinde 61,21 ve 35 degerinin oldugunu test edin.
+         */
+
+        //1- end point ve request body olustur
+
+        String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+        //2- Expected data olustur
+        //3- Request gonder ve donen response'i kaydet
+        Response response= given()
+                            .when()
+                            .get(url);
+
+        //4- Assertions
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.id", hasSize(24),
+                        "data.employee_name",hasItem("Ashton Cox"),
+                        "data.employee_age",hasItems(61,21,35));
+    }
+}
